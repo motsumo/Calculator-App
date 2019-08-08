d673a70043d5e6becfd4a3db622268b331bc7b25
motsumo <39262554+motsumo@users.noreply.github.com>
2019-08-08 23:31:22 +0300

    calculator/add : buttonAndKeypad

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
-
+import Keypad from "./components/Keypad/Keypad";
 import './App.css';
 
-class App extends Component {
+export class App extends Component {  
     render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
+      return (
+        <div className="calculator-container">
+          <Keypad/>
+        </div>
+      );
     }
-}
-
-export default App;
+  }
---
+++ src/components/Keypad/Keypad.js
@@ -0,0 +1,35 @@
+import React, {
+    Component
+} from "react";
+import {
+    keypadKeys
+} from "../../utils/constants";
+import Button from "../Button/Button";
+import './Keypad.css';
+
+
+export default class Keypad extends Component {
+    render() {
+        return ( < div className = "keypad" > {
+                keypadKeys.map((block, index) => {
+                    return ( < div key = {
+                            index
+                        }
+                        className = "block" > {
+                            block.map(key => ( < Button key = {
+                                    key
+                                }
+                                onButtonClick = {
+                                    this.handleClick
+                                }
+                                buttonKey = {
+                                    key
+                                }
+                                />
+                            ))
+                        } </div>
+                    );
+                })
+            } </div>);
+        }
+    }
